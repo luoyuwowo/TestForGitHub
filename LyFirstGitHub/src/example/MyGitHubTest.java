ec4f124441454d5436bd0abf54d25a54a4ff9a8d
luoyuwowo <34256052+luoyuwowo@users.noreply.github.com>
2017-12-05 10:11:06 +0800

    test

---
+++ LyFirstGitHub/src/example/MyGitHubTest.java
@@ -0,0 +1,22 @@
+/*
+* Copyright (C), 2002-2017, 苏宁易购电子商务有限公司
+ * FileName: MyGitHubTest.java
+ * Author:   17040294
+ * Date:     2017/12/5 9:25
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>               <time>              <version>               <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package example;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈功能详细描述〉
+ *
+ * @author 17040294
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class MyGitHubTest {
+}
